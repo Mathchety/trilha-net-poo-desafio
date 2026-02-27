2a289085aaf318ea71b02be670380b14d617f738
Matheus Marchetti <matheusmarchetti1502@gmail.com>
2026-02-27 11:22:10 -0300

    Implementando as atividades pedidas no desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,19 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
+        public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string  IMEI { get; set; }
+        public int Memoria { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Nokia: ");
+Smartphone nokia = new Nokia("998562789", "Nokia Tijolo", "15555555", 2);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WormGame");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone Iphone: ");
+Smartphone iphone = new Iphone("97548723", "Iphone 12", "17777777", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
